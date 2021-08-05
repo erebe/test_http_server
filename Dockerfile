cc69aca3cf6c9f433276f63d0be6d0d636291483
Romain GERARD <erebe@erebe.eu>
2021-08-05 13:58:56 +0200

    test

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY . .
+
+CMD [ "python3", "app.py"]
+
+
---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import http.server as SimpleHTTPServer
+import socketserver as SocketServer
+import logging
+
+PORT = 8000
+
+class GetHandler(
+        SimpleHTTPServer.SimpleHTTPRequestHandler
+        ):
+
+    def do_GET(self):
+        logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        logging.error(self.headers)
+
+
+Handler = GetHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print("Serving on port :8000")
+httpd.serve_forever()
