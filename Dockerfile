344fb7f1b01af77280ee0b19e0cfc85d8bae5b2f
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-06-01 10:02:24 +0200

    Add grpc

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ARG QOVERY_PROJECT_ID
 COPY . .
 #RUN while true; do echo sleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeping; sleep 0.1; done
 
-RUN pip install simple-websocket-server
+RUN pip install simple-websocket-server grpcio-tools
 
 CMD [ "python3", "app.py"]
 
--- app.py
+++ app.py
@@ -9,9 +9,16 @@ import sys
 import os
 from threading import Thread
 
+from concurrent import futures
+
+import grpc
+import srv_pb2
+import srv_pb2_grpc
+
 
 PORT = 8000
 WS_PORT = 8001
+GRPC_PORT = 8082
 
 class GetHandler(
         SimpleHTTPServer.SimpleHTTPRequestHandler
@@ -46,12 +53,27 @@ class SimpleEcho(WebSocket):
     def handle_close(self):
         print(self.address, 'closed')
 
+class Greeter(srv_pb2_grpc.GreeterServicer):
+   def greet(self, request, context):
+      print("Got request " + str(request))
+      return greeting_pb2.ServerOutput(message='{0} {1}!'.format(request.greeting, request.name))
+	  
+def grpc_server():
+   server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+   srv_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+   server.add_insecure_port('[::]:8082')
+   print("gRPC starting on 8082")
+   server.start()
+   server.wait_for_termination()
 
 Handler = GetHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 server = WebSocketServer('0.0.0.0', WS_PORT, SimpleEcho)
 
+
 print("Serving HTTP on port :8000")
 print("Serving Websocket on port :8001")
 Thread(target=server.serve_forever).start()
+Thread(target=grpc_server).start()
 httpd.serve_forever()
+
