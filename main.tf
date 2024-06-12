f73054322ee179396f5f5a49a56548e95a0cb054
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-06-12 16:24:24 +0200

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "qovery-erebe-test" {
+resource "aws_s3_bucket" "qovery_s3_test" {
   bucket = var.bucket_name
 
   tags = {
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "qovery-erebe-test" {
   }
 }
 
+resource "local_file" "qovery_export_to_env_var" {
+  filename = "/qovery-output/qovery-output.json"
+  content  = jsonencode({
+    "BUCKET_ARN" = { "value" = qovery_s3_test.arn, "sensitive" = false },
+    "BUCKET_DOMAIN_NAME" = { "value" = qovery_s3_test.bucket_domain_name, "sensitive" = true },
+  })
+}
