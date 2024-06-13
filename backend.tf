1ff645226fae6a1be436ef269a3e94dec9a39a81
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-06-13 15:43:53 +0200

    update

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@ terraform {
 #    access_key = "{{ aws_access_key_tfstates_account }}"
 #    secret_key = "{{ aws_secret_key_tfstates_account }}"
 #    region = "{{ aws_region_tfstates_account }}"
-#    bucket = "qovery-test-terrafom-tfstates"
-    key = "erebe.tfstate"
+#    key = "erebe.tfstate"
+    bucket = "qovery-test-terrafom-tfstates"
     dynamodb_table = "qovery-terrafom-tfstates"
   }
 }
