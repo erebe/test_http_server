ed9ecbf730c0da9243cf030b4e6dbeaef4bb4fcd
Erèbe - Romain Gerard <github@erebe.eu>
2024-07-10 17:05:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,5 +15,5 @@ output "s3_bucket_arn" {
 output "s3_bucket_domain_name" {
    value = aws_s3_bucket.qovery_s3_test.bucket_domain_name
    sensitive = true
-     description = "s3_bucket_domain_name_description"
+   description = "s3_bucket_domain_name_description"
 }
